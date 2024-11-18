ac7ddb3b822677f9efae5de8b7baa635dd2124d2
Matheus César <cesarmatt@Matheuss-MacBook-Air-2.local>
2024-11-18 10:14:48 -0300

    Adds embeddedServer configuration for future AWS beanstalk deploy

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -3,11 +3,15 @@ package com.example
 import com.example.plugins.*
 import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
 import io.ktor.server.plugins.contentnegotiation.*
 import kotlinx.serialization.json.Json
 
 fun main(args: Array<String>) {
-    io.ktor.server.netty.EngineMain.main(args)
+    embeddedServer(Netty, port = (System.getenv("PORT")?:"5000").toInt()) {
+        EngineMain.main(args)
+    }.start(wait = true)
 }
 
 fun Application.module() {
